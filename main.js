ff9018ae30a2635dae615855f5f2f031596121bd
Jeethendra Varaprasad <jeethendravaraprasad007@gmail.com>
2025-09-18 15:12:03 +0530

    Initial Commit: Developed a Random Programming Quote generator

---
+++ main.js
@@ -0,0 +1,45 @@
+const btn = document.querySelector('button');
+const quote = document.querySelector('.quote');
+const author = document.querySelector('.author');
+
+
+btn.addEventListener('click',e=>{
+    getQuote();
+});
+
+
+// Fetches quotes from the API
+async function getQuote(){
+    try{
+        const response = await fetch("https://jeetz-007.github.io/Quotes/quotes.json")
+        console.log(response)
+        if(!response.ok){
+            console.log("Error in fetching URL ")
+        }
+        const data = await response.json();
+        console.log("API Response : ",data)
+        // To debug uncomment the below code
+        // console.log(data[0].q)
+        // console.log(data[0].a)
+
+        // To generate random quotes
+        const randomIndex = Math.floor(Math.random() * data.length);
+        const selectedQuote = data[randomIndex];
+        quote.textContent = `"${selectedQuote.q}"`;
+        author.textContent = `- ${selectedQuote.a}`;
+        // quote.textContent = `${data[1].q}`;
+        // author.textContent = ` - ${data[1].a}`
+        }
+
+    catch(err){
+        console.log("Something happend : ",err)
+        quote.textContent = 'OOPS! Something went wrong'
+        author.textContent = "";
+    }
+
+}
+
+// Initial call to display a quote when the page loads
+// getQuote();
+
+
